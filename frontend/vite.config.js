e9703142c33ac8739e2b6d26189b2408231e15f6
frankwss7 <frankwss80@gmail.com>
2025-06-26 02:29:39 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react( )],
+  esbuild: {
+    target: 'esnext', // Suporte completo para ES6+
+  },
+  build: {
+    target: 'esnext', // Target também para o build
+  }
 })
