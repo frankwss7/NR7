bb3cd59e4ccc71e837411d594979e05d38ed15bf
frankwss7 <frankwss80@gmail.com>
2025-06-26 01:30:33 -0300

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import LoadingSpinner from './LoadingSpinner';
+
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
 
