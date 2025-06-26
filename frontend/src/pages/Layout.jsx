4ee943140b9d5a62d8348bb20fb6972f40a77493
frankwss7 <frankwss80@gmail.com>
2025-06-26 01:16:40 -0300

    Update Layout.jsx

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { useAuth } from '../contexts/AuthContext'; 
 import { Toaster } from 'react-hot-toast'; 
  
-const Layout = ({ children }) =
+const Layout = ({ children }) => 
   const { logout } = useAuth(); 
  
   return ( 
       {/* Sidebar */} 
-            {/* Adicione mais links de navega‡Æo aqui */} 
+            {/* Adicione mais links de navegaâ€¡Ã†o aqui */} 
  
       {/* Main Content */} 
         {/* Header */} 
-          {/* Adicione informa‡äes do usu rio ou outras a‡äes aqui */} 
+          {/* Adicione informaâ€¡Ã¤es do usuÂ rio ou outras aâ€¡Ã¤es aqui */} 
  
         {/* Page Content */} 
           {children} 
